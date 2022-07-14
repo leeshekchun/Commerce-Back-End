057639a8c32fd9f912d703fac3eaafa1c30904b5
ShekChun Lee <leeshekchun@gmail.com>
2022-07-13 19:49:42 -0600

    modified index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: ''
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: ''
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: ''
+})
 // Tags belongToMany Products (through ProductTag)
+TimeRanges.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: ''
+})
 
 module.exports = {
   Product,
