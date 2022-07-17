009e71b622db259f764449e8f4daf25cf43ecff2
ShekChun Lee <leeshekchun@gmail.com>
2022-07-16 18:28:16 -0600

    added category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,7 +34,13 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-    .then(dbProductData => res.json(dbProductData))
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.json(dbProductData);
+  })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
